da49c600dccaeb2569ff7040c899fd8523ac3b20
cyf <dvpnet@dvpnet.io>
2021-04-02 20:31:35 +0800

    Team

--- components/Team.jsx
+++ components/Team.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import Image from 'next/image'
 import styles from '../styles/Team.module.scss'
 
 const TEAM_DATA = [
   {
     name: 'Viven Kirby',
+    image: '/portrait/viven.png',
     desc: 'Cofounder & Project Lead',
   },
   {
     name: 'Norelle Ng',
+    image: '/portrait/ng.png',
     desc: 'Cofounder & Operation Lead',
   },
   {
     name: 'Burak Keçeli',
+    image: '/portrait/burak.png',
     desc: 'Cofounder & Technical Lead ',
   },
 ]
@@ -23,7 +27,8 @@ export function Team() {
       <div className={styles.people}>
         {TEAM_DATA.map((d) => {
           return (
-            <div>
+            <div className={styles.peopleSingle}>
+              <img className={styles.avatar} src={d.image} />
               <div className={styles.name}>{d.name}</div>
               <div className={styles.desc}>{d.desc}</div>
             </div>
