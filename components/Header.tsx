cb8798cee1f02b67da429449c1cef76a6ab3ae89
cyf <dvpnet@dvpnet.io>
2021-04-02 13:03:45 +0800

    new design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,14 @@
-import React, { useState, useCallback } from 'react';
-import Navigation from './Navigation';
-import MenuButton from './MenuButton';
-import styles from '../styles/Header.module.scss';
-import Logo from '../public/svg/logo.svg';
-import { HEADER_MENU } from '../constants';
+import React, { useState, useCallback } from 'react'
+import Navigation from './Navigation'
+import MenuButton from './MenuButton'
+import styles from '../styles/Header.module.scss'
+import Logo from '../public/svg/logo.svg'
+import Sakura from '../public/svg/sakura.svg'
+import { HEADER_MENU } from '../constants'
 
 const Header = () => {
-  const [active, setActive] = useState(false);
-  const toggleActive = useCallback(() => setActive(!active), [active]);
+  const [active, setActive] = useState(false)
+  const toggleActive = useCallback(() => setActive(!active), [active])
   return (
     <header className={styles.wrapper}>
       <div className={styles.container}>
@@ -15,22 +16,23 @@ const Header = () => {
           <i className={styles.logoImage}>
             <Logo />
           </i>
-          <div className={styles.logoText}>
-            clover
-          </div>
+          <div className={styles.logoText}>clover</div>
         </div>
-        <Navigation
-          items={HEADER_MENU}
-          active={active}
-        />
-        <MenuButton
-          className={styles.button}
-          active={active}
-          onClick={toggleActive}
-        />
+        <Navigation items={HEADER_MENU} active={active} />
+        <div className={styles.rightBox}>
+          <div className={styles.sakura}>
+            <Sakura />
+            <div className={styles.sakuraText}>Sakura</div>
+          </div>
+          <MenuButton
+            className={styles.button}
+            active={active}
+            onClick={toggleActive}
+          />
+        </div>
       </div>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
