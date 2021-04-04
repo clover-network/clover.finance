bfbae787ac850637f3798491d0d86bee4486d3f1
Doe Oknehchsi <ishchenko5aleksandr@gmail.com>
2021-04-04 18:09:03 +0300

    [FD-3362] Slider responsiveness

--- components/EarlyBackers/EarlyBackers.tsx
+++ components/EarlyBackers/EarlyBackers.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styles from './EarlyBackers.module.scss';
 import ArrowLeft from '../../public/svg/arrow_left.svg';
 import ArrowRight from '../../public/svg/arrow_right.svg';
@@ -6,8 +6,84 @@ import cn from 'classnames';
 import Carousel from 'react-material-ui-carousel';
 import { ThemeProvider } from '@material-ui/styles';
 import { mainTheme } from '../theme/mainTheme';
+import { useIsLgUp, useIsMdUp, useIsXsUp } from '../../utils/hooks/useTheme';
+import { uid } from 'react-uid';
+
+function chunkArrayInGroups(arr: any[], size: number) {
+  var newArray = [];
+  for (var i = 0; i < arr.length; i += size) {
+    newArray.push(arr.slice(i, i + size));
+  }
+  return newArray;
+}
+
+const BACKERS = [
+  {
+    element: <img width={123} key={1} src="/backers/bk_polychain.png" alt="" />,
+  },
+  {
+    element: <img width={123} key={2} src="/backers/bk_alameda.png" alt="" />,
+  },
+  {
+    element: (
+      <img width={123} key={3} src="/backers/bk_hypersphere.png" alt="" />
+    ),
+  },
+  {
+    element: (
+      <img width={123} key={4} src="/backers/bk_divergence.png" alt="" />
+    ),
+  },
+  {
+    element: <img width={123} key={5} src="/backers/bk_block.png" alt="" />,
+  },
+  {
+    element: <img width={123} key={6} src="/backers/bk_cms.png" alt="" />,
+  },
+  {
+    element: <img width={123} key={7} src="/backers/bk_kr1.png" alt="" />,
+  },
+  {
+    element: <img width={123} key={8} src="/backers/bk_bitcoin.png" alt="" />,
+  },
+  {
+    element: <img width={123} key={9} src="/backers/bk_moonwhale.png" alt="" />,
+  },
+  {
+    element: (
+      <img width={123} key={10} src="/backers/bk_kyrosVentures.png" alt="" />
+    ),
+  },
+  {
+    element: <img width={123} key={11} src="/backers/bk_okex.png" alt="" />,
+  },
+];
 
 const EarlyBackers = ({ className }: { className?: string }) => {
+  const isXsUp = useIsXsUp();
+  const isMdUp = useIsMdUp();
+  const isLgUp = useIsLgUp();
+
+  const elements = useMemo(() => {
+    const chunks = (() => {
+      if (isLgUp) {
+        return chunkArrayInGroups(BACKERS, 4);
+      }
+      if (isMdUp) {
+        return chunkArrayInGroups(BACKERS, 3);
+      }
+      return chunkArrayInGroups(BACKERS, 1);
+    })();
+
+    return chunks.map(backers => {
+      return (
+        <div key={uid(backers)} className={styles.sliderContentWrapper}>
+          {backers.map(item => item.element)}
+        </div>
+      );
+    });
+  }, [isXsUp, isMdUp, isLgUp]);
+
   return (
     <ThemeProvider theme={mainTheme}>
       <div className={styles.wrapper}>
@@ -33,81 +109,8 @@ const EarlyBackers = ({ className }: { className?: string }) => {
                 },
                 className: '',
               }}
-              interval={1000000}
             >
-              <div className={styles.sliderContentWrapper}>
-                <img
-                  className={styles.image}
-                  width={103}
-                  src="/backers/bk_polychain.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={226}
-                  src="/backers/bk_alameda.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={168}
-                  src="/backers/bk_hypersphere.png"
-                  alt=""
-                />
-
-                <img
-                  className={styles.image}
-                  width={123}
-                  src="/backers/bk_divergence.png"
-                  alt=""
-                />
-              </div>
-              <div className={styles.sliderContentWrapper}>
-                <img
-                  className={styles.image}
-                  width={123}
-                  src="/backers/bk_block.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={125}
-                  src="/backers/bk_cms.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={125}
-                  src="/backers/bk_kr1.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={125}
-                  src="/backers/bk_bitcoin.png"
-                  alt=""
-                />
-              </div>
-              <div className={styles.sliderContentWrapper}>
-                <img
-                  className={styles.image}
-                  width={125}
-                  src="/backers/bk_moonwhale.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={125}
-                  src="/backers/bk_kyrosVentures.png"
-                  alt=""
-                />
-                <img
-                  className={styles.image}
-                  width={113}
-                  src="/backers/bk_okex.png"
-                  alt=""
-                />
-              </div>
+              {elements}
             </Carousel>
           </div>
         </div>
--- components/theme/mainTheme.tsx
+++ components/theme/mainTheme.tsx
@@ -3,6 +3,15 @@ import { createMuiTheme } from '@material-ui/core';
 import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
 
 const mainTheme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+    },
+  },
   props: {
     MuiIconButton: {
       disableRipple: true,
