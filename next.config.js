5202271bd2f136038c74a29655e4cc36394af6c6
Doe Oknehchsi <ishchenko5aleksandr@gmail.com>
2021-04-04 16:10:09 +0300

    [FD-3362] withPlugins

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-const path = require('path')
-const withReactSvg = require('next-react-svg')
+const path = require('path');
+const withReactSvg = require('next-react-svg');
+const withPlugins = require('next-compose-plugins');
 
-module.exports = withReactSvg({
-  include: path.resolve(__dirname, 'public/svg'),
-  webpack(config, options) {
-    return config
-  },
-})
+module.exports = withPlugins([
+  [
+    withReactSvg,
+    {
+      include: path.resolve(__dirname, 'public/svg'),
+      webpack(config, options) {
+        return config;
+      },
+    },
+  ],
+]);
