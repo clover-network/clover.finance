1e8b06d2ebef88453a1da963bc8ac45fca62094d
Deyang Liu <ldy0909061@gmail.com>
2022-03-22 17:43:36 +0800

    Add ga code

--- src/DefaultHead.tsx
+++ src/DefaultHead.tsx
@@ -34,6 +34,16 @@ export const DefaultHead = () => {
           gtag('config', 'UA-217926805-1');`
         }}
       />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-3859X4XY4M"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-3859X4XY4M');`
+        }}
+      />
     </Head>
   );
 };
