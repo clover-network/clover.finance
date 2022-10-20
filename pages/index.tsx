dc92e7f1696e5e091cad8d1dd892566fdebd66e2
Taneeya <2240351540@qq.com>
2022-10-20 18:43:34 +0800

    add redirect for phone and pad

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,34 @@
-import React from "react";
-import { SplashModeContext, SplashPageMode } from "../src/SplashModeContext";
-import { GlobalStyle } from "../src/GlobalStyle";
-import { DefaultHead } from "../src/DefaultHead";
-import { AppBase } from '../src/layout/AppBase/AppBase';
-import { NewContent } from '../src/pages/newContent';
+import React, {useEffect} from "react";
+import {SplashModeContext, SplashPageMode} from "../src/SplashModeContext";
+import {GlobalStyle} from "../src/GlobalStyle";
+import {DefaultHead} from "../src/DefaultHead";
+import {AppBase} from '../src/layout/AppBase/AppBase';
+import {NewContent} from '../src/pages/newContent';
 import store from '../src/state';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 
 export default function FirstPost() {
-  return (
-    <SplashModeContext.Provider value={SplashPageMode.CLOVER}>
-      <Provider store={store}>
-        <AppBase>
-          <DefaultHead />
-          <GlobalStyle />
-          <NewContent />
-        </AppBase>
-      </Provider>
-    </SplashModeContext.Provider>
-  );
+    useEffect(() => {
+        const ua = navigator.userAgent
+        const isWindowsPhone = /(?:Windows Phone)/.test(ua)
+        const isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone
+        const isAndroid = /(?:Android)/.test(ua)
+        const isTablet = /(?:iPad|PlayBook)/.test(ua) || (isAndroid && !/(?:Mobile)/.test(ua))
+        const isPhone = /(?:iPhone)/.test(ua) && !isTablet
+        const isPc = !isPhone && !isAndroid && !isSymbian
+        if (!isPc) {
+            window.open('https://m.clv.org/', '_self')
+        }
+    }, [])
+    return (
+        <SplashModeContext.Provider value={SplashPageMode.CLOVER}>
+            <Provider store={store}>
+                <AppBase>
+                    <DefaultHead/>
+                    <GlobalStyle/>
+                    <NewContent/>
+                </AppBase>
+            </Provider>
+        </SplashModeContext.Provider>
+    );
 }
