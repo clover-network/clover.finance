da553dda46033b2906acf32691979f2dfb36754e
Sophie <sophie4140@outlook.com>
2022-11-03 00:50:56 +0800

    fix: Added object sorting fixes issues with media query

--- src/mixins/breakpoint.ts
+++ src/mixins/breakpoint.ts
@@ -1,24 +1,28 @@
-import { css, FlattenInterpolation, DefaultTheme } from "styled-components";
+import { css, FlattenInterpolation, DefaultTheme } from 'styled-components';
 
 type InputType = Record<
   keyof DefaultTheme['breakpoints'],
   FlattenInterpolation<any>
 >;
 
+// TODO: 加类型
+const sorted = ['mobile', 'desktop'];
 export const breakpoint = (input: InputType | FlattenInterpolation<any>) => {
   if (Object.prototype.hasOwnProperty.call(input, 'mobile')) {
-    const sortedInputList = Object.entries(input).sort();
-    const query = (props: any) => sortedInputList.reduce((q, [key, value]) => {
-      q += `  
+    const query = (props: any) =>
+      sorted.reduce((q, key) => {
+        // @ts-ignore
+        const value = (input as InputType)[key];
+        q += `  
     @media screen and (max-width: ${props.theme.breakpoints[key]}) {
       ${value}
     }
     `;
-      return q;
-    }, '');
+        return q;
+      }, '');
     return css`
       ${query}
-    `
+    `;
   }
 
   return css`
@@ -26,4 +30,4 @@ export const breakpoint = (input: InputType | FlattenInterpolation<any>) => {
       ${input as any}
     }
   `;
-}
+};
